45f68562528a7d889c274fdc5d64bc1d18c933ef
tmoechel-futura <t.moechel@futura-solutions.de>
2023-07-17 18:27:36 +0200

    Projektdateien hinzufügen.

---
+++ SimpleDBAccess/Domain/RunActivity.cs
@@ -0,0 +1,9 @@
+﻿namespace EFC6.Ch2.FitnessApp.Domain
+{
+    public class RunActivity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Distance { get; set; }
+    }
+}
---
+++ SimpleDBAccess/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EFC6.Ch2.FitnessApp.Domain;
+using Microsoft.Data.SqlClient;
+
+List<RunActivity> runActivities = new List<RunActivity>();
+
+ReadRunActivitiesFromDb();
+ShowRunActivities();
+void ReadRunActivitiesFromDb()
+{
+    string connectionString = @"Server = localhost\SQLEXPRESS; Database=FitnessDb; Trusted_Connection = True; TrustServerCertificate=True";
+
+    var sqlConnection = new SqlConnection(connectionString);
+
+    string sql = "select Id, Name, Distance from RunActivities";
+
+    sqlConnection.Open();
+
+    using (SqlCommand command = new SqlCommand(sql, sqlConnection))
+    {
+        using (SqlDataReader reader = command.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                var runActivity = new RunActivity() { Id = reader.GetInt32(0), Name = reader.GetString(1), Distance = reader.GetDouble(2) };
+                runActivities.Add(runActivity);
+            }
+        }
+    }
+}
+
+void ShowRunActivities()
+{
+    foreach (var runActivity in runActivities)
+    {
+        Console.WriteLine($"Name: {runActivity.Name} Distance: {runActivity.Distance}");
+    }
+}
+
+
+
